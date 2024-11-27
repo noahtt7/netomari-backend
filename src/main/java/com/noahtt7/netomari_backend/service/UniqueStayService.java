4c31b09a14c7caf7caf14c785adc5da265b9abbe
noahtt7 <noahtaguma@gmail.com>
2024-11-27 11:38:43 -0800

    Created service layer for uniquestays and stay service implementation
    Created UniqueStayService and StayServiceImpl, which has a method called createUniqueStay. The method converts given UniqueStayDto to a UniqueStay entity, saves it to database, and returns saved uniquestay back to client.

---
+++ src/main/java/com/noahtt7/netomari_backend/service/UniqueStayService.java
@@ -0,0 +1,9 @@
+package com.noahtt7.netomari_backend.service;
+
+import com.noahtt7.netomari_backend.dto.UniqueStayDto;
+
+public interface UniqueStayService {
+    UniqueStayDto createUniqueStay(UniqueStayDto uniqueStayDto);
+    
+    
+}
---
+++ src/main/java/com/noahtt7/netomari_backend/service/impl/StayServiceImpl.java
@@ -0,0 +1,11 @@
+package com.noahtt7.netomari_backend.service.impl;
+
+import com.noahtt7.netomari_backend.dto.UniqueStayDto;
+import com.noahtt7.netomari_backend.service.UniqueStayService;
+
+public class StayServiceImpl implements UniqueStayService {
+    @Override
+    public UniqueStayDto createUniqueStay(UniqueStayDto uniqueStayDto) {
+        return null;
+    }
+}
